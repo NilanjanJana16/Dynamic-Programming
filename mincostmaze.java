9cd1774bdeb820182c387ee30e23614c56da113c
NilanjanJana16 <nildbs16@gmail.com>
2022-04-21 13:55:54 +0530

    1st commit

--- mincostmaze.java
+++ mincostmaze.java
@@ -25,7 +25,7 @@ public class mincostmaze {
                 if(i == dp.length-1 && j == dp[0].length-1){
                     dp[i][j] = arr[i][j];
                 }else if(i == dp.length-1){
-                    dp[i][j] = dp[i][j+1] + arr[i][j];
+                    dp[i][j] = dp[i][j+1] + arr[i][j];-aq 
                 }else if(j == dp[0].length-1){
                     dp[i][j] = dp[i+1][j] + arr[i][j];
                 }else{
